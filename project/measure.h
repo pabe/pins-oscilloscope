bc755afda5638a09213fb53b85dd0d578155fc76
Markus Hedlund <hedlund.markus@gmail.com>
2011-05-12 23:00:16 +0200

    Second version of measure

--- project/measure.h
+++ project/measure.h
@@ -1,19 +1,15 @@
 #ifndef __MEASURE_H_
 #define __MEASURE_H_
-
 #define NUMBER_OF_CHANNELS 2  //Number of probes on the oscilloscope
-
 
 typedef struct {
   ADC_TypeDef* ADC;		  // ADC to use
-  uint8_t ADC_Channel;	  //Channel to use.. (ADC_Channel_0, ADC_Channel_1 ...
-
+  uint8_t ADC_Channel;	  // ADC Channel to use.. (ADC_Channel_0, ADC_Channel_1 ... 17
   int rate;               // Sample rate in Herz
-  int subscribed;		  // 1 if has subscribers.
+  int subscribed;		  // 1 if has channel has subscribers.
   int inputChannel;		  // Input number "seen" on the oscilloscope.
 } OscilloscopeChannel;
 
-OscilloscopeChannel oChan[NUMBER_OF_CHANNELS] ;
-
+OscilloscopeChannel oChan[NUMBER_OF_CHANNELS];
 
 #endif /* __MEASURE_H_ */

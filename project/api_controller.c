c6b3ed322780fad756d0287aa534f8dacfac08b6
Patrik Berglund <pabe3179@student.uu.se>
2011-06-08 10:03:16 +0200

    Deinline and namechange

--- project/api_controller.c
+++ project/api_controller.c
@@ -1,3 +1,34 @@
 #include "api_controller.h"
 
 xQueueHandle ipc_controller;
+
+portBASE_TYPE ipc_controller_mode_set(oscilloscope_mode_t mode)
+{
+  msg_controller_cmd_t cmd;
+  switch(mode)
+  {
+    case oscilloscope_mode_oscilloscope:
+      cmd = controller_cmd_set_mode_oscilloscope;
+      break;
+
+    case oscilloscope_mode_multimeter:
+      cmd = controller_cmd_set_mode_multimeter;
+      break;
+
+    default:
+      return pdFALSE;
+  }
+
+  return ipc_controller_send_cmd(cmd);
+}
+
+__inline portBASE_TYPE ipc_controller_send_cmd(msg_controller_cmd_t cmd)
+{
+  msg_t msg;
+  msg.head.id = msg_id_controller_cmd;
+  msg.data.controller_cmd = cmd;
+
+  assert(ipc_controller);
+  return xQueueSendToBack(ipc_controller, &msg, portMAX_DELAY);
+}
+

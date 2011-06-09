f566e5bf9964126cfc0c8d6340213a3ae5ca316c
Markus Hedlund <hedlund.markus@gmail.com>
2011-06-09 15:09:36 +0200

    some adc-work

--- project/measure.c
+++ project/measure.c
@@ -9,24 +9,25 @@
 #include "queue.h"
 #include "stm3210c_eval_ioe.h"
 #include "stm32f10x_adc.h"
+#include "api_controller.h"
 
 
 
 int samplerate = 50;
 OscilloscopeChannel oChan[NUMBER_OF_CHANNELS];
 
-int setSampleRate(int rate, int channel){
+int setSampleRate(int rate, oscilloscope_input_t channel){
 	int cntr = 0;
  	while(cntr < NUMBER_OF_CHANNELS){
 		if(oChan[cntr].inputChannel == channel)
 			oChan[cntr].rate=rate;
 		cntr++;
  	}
- 	return 0;
+ 	return pdTRUE;;
 } 
 
 
-int getSampleRate(int channel){
+int getSampleRate(oscilloscope_input_t channel){
 	int cntr = 0;
  	while(cntr < NUMBER_OF_CHANNELS){
 		if(oChan[cntr].inputChannel == channel)
@@ -36,7 +37,7 @@ int getSampleRate(int channel){
  	return -1;
 } 
 
-int setSubscribe(int subscribe, int channel){
+int setSubscribe(int subscribe, oscilloscope_input_t channel){
 	
 	int cntr = 0;
 	while(cntr < NUMBER_OF_CHANNELS){
@@ -44,10 +45,10 @@ int setSubscribe(int subscribe, int channel){
 			oChan[cntr].subscribed=subscribe;
 		cntr++;
  	}
- 	return 0;
+ 	return pdTRUE;;
 } 
 
-int getSubscribe(int channel){
+int getSubscribe(oscilloscope_input_t channel){
 	
 	int cntr = 0;
 	while(cntr < NUMBER_OF_CHANNELS){
@@ -58,7 +59,7 @@ int getSubscribe(int channel){
  	return -1;
 } 
 
-double voltageConversion(int val){
+double voltageConversion(uint16_t val){
 	double maxAdcBits = 4095.0; // Should be > 0 
 	double maxVolts = 3.3;      
 	double voltsPerBit = (maxVolts / (maxAdcBits));
@@ -74,7 +75,7 @@ portTickType herzToTicks(int samplerate){
 		 return (portTickType)period_in_MS / portTICK_RATE_MS;
 }
 
-int readChannel(OscilloscopeChannel OChannel){
+uint16_t readChannel(OscilloscopeChannel OChannel){
 		ADC_RegularChannelConfig(OChannel.ADC,OChannel.ADC_Channel, 1, ADC_SampleTime_239Cycles5);
 		ADC_ClearFlag(OChannel.ADC, ADC_FLAG_EOC);
 		ADC_SoftwareStartConvCmd(OChannel.ADC, ENABLE);
@@ -84,11 +85,12 @@ int readChannel(OscilloscopeChannel OChannel){
 }	
 
 void measureTask (void* params) {
-	int voltage,  i;
-
+	int  packetCounter, i;
+	//oscilloscope_input_t i;
+    uint16_t adc_value;
     portTickType xLastWakeTime;
     portTickType xFrequency = herzToTicks(samplerate);
-
+	packetCounter = 0;
 
 	for(;;){
 
@@ -97,20 +99,25 @@ void measureTask (void* params) {
 	xLastWakeTime = xTaskGetTickCount();
      
 
-
+	
 	  
 	 for (i = 0; i < NUMBER_OF_CHANNELS;i++){
-			setSubscribe(1, i);  //Should be set by value from ipc FIX
-			setSampleRate(samplerate, i); //Should be set by value from ipc FIX
+			setSubscribe(1, oChan[i].inputChannel);  //Should be set by value from ipc FIX
+			setSampleRate(samplerate, oChan[i].inputChannel); //Should be set by value from ipc FIX
 		
 			
 			
-			voltage = readChannel(oChan[i]);
-			printf("%f \n", voltageConversion(voltage));
+			adc_value = readChannel(oChan[i]);
+			printf("%.2f ", voltageConversion(adc_value));
+
+			//if(!ipc_controller_send_data(oChan[i].inputChannel,adc_value,packetCounter));
+			 // assert(0);
+
+
 		}
 
 
-
+		 packetCounter++;
 		
 		//printf("xLastWakeTime %d \n", xLastWakeTime);
 		
@@ -151,21 +158,21 @@ void measureTask (void* params) {
 		assert(0); //this need to be generalized if other amount of chans is needed;
 									   
 	 oChan[0].ADC=ADC1;
-	 oChan[0].ADC_Channel=ADC_Channel_0;
+	 oChan[0].ADC_Channel=ADC_Channel_7;
      oChan[0].subscribed=	0;
-	 oChan[0].inputChannel = 1;
+	 oChan[0].inputChannel = input_channel0;
      oChan[0].rate=50;
 
 	 oChan[1].ADC=ADC1;
-	 oChan[1].ADC_Channel=ADC_Channel_1;
+	 oChan[1].ADC_Channel=ADC_Channel_8;
      oChan[1].subscribed=	0;
-	 oChan[1].inputChannel = 2;
+	 oChan[1].inputChannel = input_channel1;
      oChan[1].rate=50;
 
   
-   xTaskCreate(measureTask,"",100, NULL, 1, NULL);
+   //xTaskCreate(measureTask,"",100, NULL, 1, NULL);
    
 
-  assert(0);
+  
 
 }
--- project/measure.h
+++ project/measure.h
@@ -1,6 +1,7 @@
 #ifndef __MEASURE_H_
 #define __MEASURE_H_
 #include "stm32f10x_adc.h"
+#include "oscilloscope.h"
 #define NUMBER_OF_CHANNELS 2  //Number of probes on the oscilloscope
 
 #define ADC_MAX ((1<<12)-1)
@@ -10,14 +11,14 @@ typedef struct {
   uint8_t ADC_Channel;	  // ADC Channel to use.. (ADC_Channel_0, ADC_Channel_1 ... 17
   int rate;               // Sample rate in Herz
   int subscribed;		  // 1 if has channel has subscribers.
-  int inputChannel;		  // Input number "seen" on the oscilloscope.
+  oscilloscope_input_t inputChannel;		  // Input number "seen" on the oscilloscope.
 } OscilloscopeChannel;
 
 void measureInit(void);
-
-int setSampleRate(int rate, int channel);
-int getSampleRate(int channel);
-int setSubscribe(int subscribe, int channel);
-int getSubscribe(int channel);
+void measureTask (void* params);
+int setSampleRate(int rate, oscilloscope_input_t channel);
+int getSampleRate(oscilloscope_input_t channel);
+int setSubscribe(int subscribe, oscilloscope_input_t channel);
+int getSubscribe(oscilloscope_input_t channel);
 
 #endif /* __MEASURE_H_ */

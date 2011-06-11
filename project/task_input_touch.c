5cd75790620329073cb2b249f513027faf66f4c5
Magnus Söderling <magnus.soderling@gmail.com>
2011-06-11 16:22:24 +0200

    basic btnhandler

--- project/task_input_touch.c
+++ project/task_input_touch.c
@@ -1,10 +1,6 @@
 /*
  * Input driver for touchscreen.
  */
-
-
-
-
 #include <stdio.h>
 
 /* Firmware */
@@ -15,6 +11,7 @@
 #include "GLCD.h"
 #include "stm3210c_eval_ioe.h"
 #include "task.h"
+#include "semphr.h"
 
 #include "api_input_touch.h"
 #include "api_controller.h"
@@ -22,10 +19,9 @@
 #include "task_input_touch.h"
 #include "oscilloscope.h"
 
- /*--- ONLY for testing purposes remove for release Drawing in this task---*/
-#include "semphr.h"	 //FIXME
 
- /*-------------------------------------*/
+
+/*-------------------------------------*/
 
 #define WIDTH 320 
 
@@ -35,6 +31,8 @@
 static oscilloscope_mode_t mode;
 extern xSemaphoreHandle lcdLock;
 /* private functions */
+static void btnPressHandler(void *data);
+static void registerButtonsCallback(void);
 /*-----------------------------------------------------------*/
 
 /**
@@ -47,110 +45,117 @@ extern xSemaphoreHandle lcdLock;
  */
 
 typedef struct {
-  u16 lower, upper, left, right;
-  void *data;
-  void (*callback)(void *data);
+    u16 lower, upper, left, right;
+    void *data;
+    void (*callback)(void *data);
 } TSCallback;
 
 static TSCallback callbacks[16];
 static u8 callbackNum = 0;
 
 void registerTSCallback(u16 left, u16 right, u16 lower, u16 upper,
-                        void (*callback)(void *data),
-						void *data) {
-  callbacks[callbackNum].lower    = lower;
-  callbacks[callbackNum].upper    = upper;
-  callbacks[callbackNum].left     = left;
-  callbacks[callbackNum].right    = right;
-  callbacks[callbackNum].callback = callback;
-  callbacks[callbackNum].data     = data;
-  callbackNum++;
+        void (*callback)(void *data), void *data) {
+    callbacks[callbackNum].lower = lower;
+    callbacks[callbackNum].upper = upper;
+    callbacks[callbackNum].left = left;
+    callbacks[callbackNum].right = right;
+    callbacks[callbackNum].callback = callback;
+    callbacks[callbackNum].data = data;
+    callbackNum++;
 }
 
-static void printButton(void *data){
-  u16 button;
-  button = (int)data;
-  printf("I am button:%d Mode:%d", button, mode);
+static void btnPressOscMode(u16 btn) {
+    printf("btn:%d Mode:%d", btn, mode);
 }
 
-static void setupButtons(void) { 
-  //extern xSemaphoreHandle lcdLock; //FIXME
-  u16 i; 
-  xSemaphoreTake(lcdLock, portMAX_DELAY);
-  for (i = 0; i < 3; ++i) { 
-    GLCD_drawRect(30 + 60*i, 30, 40, 40); 
-	registerTSCallback(WIDTH - 30 - 40, WIDTH - 30, 30 + 60*i + 40, 30 + 60*i, 
-	                   &printButton, (void*)i); 
-  }
-  xSemaphoreGive(lcdLock); //FIXME
-} 
+static void btnPressVmMode(u16 btn) {
+    printf("btn:%d Mode:%d", btn, mode);
+}
+
+static void registerButtonsCallback(void) {
+    u16 i;
+    xSemaphoreTake(lcdLock, portMAX_DELAY);
+    for (i = 0; i < 6; ++i) {
+        GLCD_drawRect(0 + 40 * i, 30, 40, 40);
+        registerTSCallback(WIDTH - 30 - 40, WIDTH - 30, 0 + 40 * i + 40, 0 + 40 * i,
+                &btnPressHandler, (void*) i);
+    }
+    xSemaphoreGive(lcdLock);
+}
+
+static void btnPressHandler(void *data) {
+    u16 button;
+    button = (int) data;
+    //printf("btn:%d Mode:%d", button, mode);
+    switch (mode) {
+        case oscilloscope_mode_oscilloscope:
+            btnPressOscMode(button);
+            break;
+
+        case oscilloscope_mode_multimeter:
+            btnPressVmMode(button);
+            break;
+    }
+}
 
 /* public functions */
-void task_input_touch(void *p)
-{
-  TS_STATE *ts_state;
-  static u8 pressed = 0;
-  static u8 i;
-  portTickType timeout = CFG_TASK_INPUT_TOUCH__POLLING_PERIOD;
+void task_input_touch(void *p) {
+    TS_STATE *ts_state;
+    static u8 pressed = 0;
+    static u8 i;
+    portTickType timeout = CFG_TASK_INPUT_TOUCH__POLLING_PERIOD;
 
-  /* subscribe to mode variable in the controller, returns pd(TRUE|FALSE) */
-  ipc_controller_subscribe(ipc_input_touch, ipc_controller_variable_mode);
-  setupButtons();
+    /* subscribe to mode variable in the controller, returns pd(TRUE|FALSE) */
+    ipc_controller_subscribe(ipc_input_touch, ipc_controller_variable_mode);
+    registerButtonsCallback();
 
-  while(1)
-  {
-    portTickType sleep_time;
-    msg_t msg;
+    while (1) {
+        portTickType sleep_time;
+        msg_t msg;
 
-    sleep_time = xTaskGetTickCount();
-    
-    assert(ipc_input_touch);
-    if(pdFALSE == xQueueReceive(ipc_input_touch, &msg, timeout))
-    {
-      /* timeout work */
-	xSemaphoreTake(lcdLock, portMAX_DELAY);
-	ts_state = IOE_TS_GetState();
-	xSemaphoreGive(lcdLock); //FIXME
+        sleep_time = xTaskGetTickCount();
 
-	if (pressed) {
-	  if (!ts_state->TouchDetected)
-	    pressed = 0;
-	} else if (ts_state->TouchDetected) {
-	  for (i = 0; i < callbackNum; ++i) {
-		if (callbacks[i].left  <= ts_state->X &&
-		    callbacks[i].right >= ts_state->X &&
-		    callbacks[i].lower >= ts_state->Y &&
-		    callbacks[i].upper <= ts_state->Y){
-		       callbacks[i].callback(callbacks[i].data);
-					 }
-	  }													
-	  pressed = 1;
-	}
+        assert(ipc_input_touch);
+        if (pdFALSE == xQueueReceive(ipc_input_touch, &msg, timeout)) {
+            /* timeout work */
+            xSemaphoreTake(lcdLock, portMAX_DELAY);
+            ts_state = IOE_TS_GetState();
+            xSemaphoreGive(lcdLock);
 
-    if (ts_state->TouchDetected) {
-	  //printf("%d,%d,%d ", ts_state->X, ts_state->Y, ts_state->Z);
-	}
+            if (pressed) {
+                if (!ts_state->TouchDetected)
+                    pressed = 0;
+            } else if (ts_state->TouchDetected) {
+                for (i = 0; i < callbackNum; ++i) {
+                    if (callbacks[i].left <= ts_state->X &&
+                            callbacks[i].right >= ts_state->X &&
+                            callbacks[i].lower >= ts_state->Y &&
+                            callbacks[i].upper <= ts_state->Y) {
+                        callbacks[i].callback(callbacks[i].data);
+                    }
+                }
+                pressed = 1;
+            }
 
+            /*if (ts_state->TouchDetected) {
+                } */
 
+        } else {
+            switch (msg.head.id) {
+                case msg_id_subscribe_mode:
+                    //printf("| MODE: %i |", msg.data.subscribe_mode);
+                    mode = msg.data.subscribe_mode;
+                    //printf("mymode:%d ", mode);
+                    break;
+
+                default:
+                    ipc_watchdog_signal_error(0);
+            }
+
+            /* recalculate timeout */
+            timeout = timeout - (xTaskGetTickCount() - sleep_time);
+        }
     }
-    else
-    {
-      switch(msg.head.id)
-      {
-        case msg_id_subscribe_mode:
-          //printf("| MODE: %i |", msg.data.subscribe_mode);
-		  mode = msg.data.subscribe_mode;
-		  //printf("mymode:%d ", mode);
-          break;
-
-        default:
-          ipc_watchdog_signal_error(0);
-      }
-
-      /* recalculate timeout */
-      timeout = timeout - (xTaskGetTickCount() - sleep_time);
-    }
-  }
 }
 
 /* private functions */

c2564188be36ce60d0388fa90f4a55ef14497bf2
Erik Hansson <dev@elipsion.se>
2011-06-11 11:40:08 +0200

    Display off-by-one & types

--- project/task_display.c
+++ project/task_display.c
@@ -6,9 +6,9 @@
 #include "oscilloscope.h"
 #include "GLCD.h"
 
-portBASE_TYPE display_buffer[DISPLAY_BUFF_SIZE][NUMBER_OF_CHANNELS] = {0};
-portBASE_TYPE display_buffer_index[NUMBER_OF_CHANNELS] = {0};
-portBASE_TYPE display_buffer_enable[NUMBER_OF_CHANNELS] = {0};
+uint16_t display_buffer[DISPLAY_BUFF_SIZE][NUMBER_OF_CHANNELS] = {0};
+int display_buffer_index[NUMBER_OF_CHANNELS] = {0};
+char display_buffer_enable[NUMBER_OF_CHANNELS] = {0};
 
 /* private functions */
 static portBASE_TYPE handle_msg_subscribe_mode(msg_id_t id, msg_data_t *data);
@@ -28,9 +28,9 @@ static const ipc_loop_t msg_handle_table[] =
 
 void task_display(void *args)
 {
-  ipc_controller_subscribe(ipc_display, ipc_controller_variable_mode);
-  ipc_measure_subscribe(ipc_display, ipc_measure_variable_data_ch0);
-  ipc_measure_subscribe(ipc_display, ipc_measure_variable_data_ch1);
+	ipc_controller_subscribe(ipc_display, ipc_controller_variable_mode);
+	ipc_measure_subscribe(ipc_display, ipc_measure_variable_data_ch0);
+	ipc_measure_subscribe(ipc_display, ipc_measure_variable_data_ch1);
 
 	while(1)
   {
@@ -53,46 +53,47 @@ void task_display(void *args)
 }
 
 /* private functions */
+#define display_index(channel) (display_buffer_index[channel] % DISPLAY_BUFF_SIZE)
 static portBASE_TYPE handle_msg_subscribe_mode(msg_id_t id, msg_data_t *data)
 {
 	display_mode = data->subscribe_mode;
 	display_redraw();
 #if 0
-  switch(data->subscribe_mode)
-  {
-    case oscilloscope_mode_oscilloscope:
-      printf("|LOL0|");
-      break;
+	switch(data->subscribe_mode)
+	{
+		case oscilloscope_mode_oscilloscope:
+			printf("|LOL0|");
+			break;
 
-    case oscilloscope_mode_multimeter:
-      printf("|LOL1)");
-      break;
+		case oscilloscope_mode_multimeter:
+			printf("|LOL1)");
+			break;
 
-    default:
-      return pdFALSE;
-  }
+		default:
+			return pdFALSE;
+	}
 #endif
 
-  return pdTRUE;
+	return pdTRUE;
 }
 
 static portBASE_TYPE handle_msg_subscribe_measure_data(msg_id_t id, msg_data_t *data)
 {
-  switch(data->subscribe_measure_data.ch)
-  {
-    case input_channel0:
-		display_new_measure(0, data->subscribe_measure_data.data, data->subscribe_measure_data.timestamp);
-      break;
+	switch(data->subscribe_measure_data.ch)
+	{
+		case input_channel0:
+			display_new_measure(0, data->subscribe_measure_data.data, data->subscribe_measure_data.timestamp);
+			break;
 
-    case input_channel1:
-		display_new_measure(1, data->subscribe_measure_data.data, data->subscribe_measure_data.timestamp);
-      break;
+		case input_channel1:
+			display_new_measure(1, data->subscribe_measure_data.data, data->subscribe_measure_data.timestamp);
+			break;
 
-    default:
-      return pdFALSE;
-  }
+		default:
+			return pdFALSE;
+	}
 
-  return pdTRUE;
+	return pdTRUE;
 }
 
 static portBASE_TYPE handle_msg_cmd(msg_id_t id, msg_data_t *data)
@@ -110,24 +111,25 @@ static portBASE_TYPE handle_msg_cmd(msg_id_t id, msg_data_t *data)
 }
 static portBASE_TYPE handle_msg_toggle_channel(msg_id_t id, msg_data_t *data)
 {
-  switch(data->msg_display_toggle_channel)
-  {
-    case input_channel0:
-		display_buffer_enable[0] ^= 1;
-      break;
-    case input_channel1:
-		display_buffer_enable[1] ^= 1;
-      break;
-    default:
-      return pdFALSE;
-  }
+	switch(data->msg_display_toggle_channel)
+	{
+		case input_channel0:
+			display_buffer_enable[0] ^= 1;
+			break;
+		case input_channel1:
+			display_buffer_enable[1] ^= 1;
+			break;
+		default:
+			return pdFALSE;
+	}
 
-  return pdTRUE;
+	return pdTRUE;
 }
 
 // Call when layout has changed
 void display_redraw(void) {
-	int channel, sample; //C89  ;__;
+	char channel;
+	uint16_t sample; //C89  ;__;
 	switch(display_mode) {
 		case oscilloscope_mode_oscilloscope:
 			// Draw interface
@@ -136,7 +138,7 @@ void display_redraw(void) {
 			for(channel = 0; channel < NUMBER_OF_CHANNELS; channel++) {
 				// Check if channel active?
 				for(sample = 0; sample < DISPLAY_BUFF_SIZE; sample++) {
-					display_sample(channel, display_buffer[display_buffer_index[channel]][channel]);
+					display_sample(channel, display_buffer[sample][channel]);
 				}
 			}
 			break;
@@ -150,11 +152,11 @@ void display_redraw(void) {
 	}
 }
 
-void display_new_measure(portBASE_TYPE channel, portBASE_TYPE sample, portBASE_TYPE timestamp) {
+void display_new_measure(char channel, uint16_t sample, int timestamp) {
 	switch(display_mode) {
 		case oscilloscope_mode_oscilloscope: 
 			do {
-				if((display_buffer_index[channel] +1) == timestamp)
+				if((display_buffer_index[channel]) == timestamp)
 					display_sample(channel, sample);
 				else
 					display_sample(channel, 0);
@@ -171,36 +173,35 @@ void display_new_measure(portBASE_TYPE channel, portBASE_TYPE sample, portBASE_T
 
 
 // New sample to display, regardless of mode
-void display_sample(portBASE_TYPE channel, portBASE_TYPE sample) {
+void display_sample(char channel, uint16_t sample) {
 	switch(display_mode) {
 		// Draw new pixel
 		case oscilloscope_mode_oscilloscope:
 			// Remove old pixel
 			GLCD_setTextColor(Black);
-			display_show_analog(display_buffer_index[channel], display_buffer[display_buffer_index[channel]][channel]);
+			display_show_analog(display_index(channel), display_buffer[display_index(channel)][channel]);
 
 			// Display new pixel
 			GLCD_setTextColor(channel ? Green : Magenta);
-			display_show_analog(display_buffer_index[channel], sample);
+			display_show_analog(display_index(channel), sample);
 
 			// Update buffer
-			display_buffer[display_buffer_index[channel]][channel] = sample;
+			display_buffer[display_index(channel)][channel] = sample;
 			display_buffer_index[channel]++;
-			display_buffer_index[channel] %= DISPLAY_BUFF_SIZE;
 
 			break;
 
-		// Update number
+			// Update number
 		case oscilloscope_mode_multimeter:
 			break;
 
-		// Whoops?
+			// Whoops?
 		default:
 			break;
 	}
 }
 
-void display_show_analog(portBASE_TYPE x, portBASE_TYPE y) {
+void display_show_analog(uint16_t x, uint16_t y) {
 	// Stupid display has all messed up coordinate system :/
 	// We pretend this is not the case...
 	//          <- y
@@ -209,8 +210,8 @@ void display_show_analog(portBASE_TYPE x, portBASE_TYPE y) {
 	// |           | v
 	// |           |
 	// +-----------+
-	portBASE_TYPE disx = (DISPLAY_Y_RES - DISPLAY_MENU_HEIGHT) - y / (ADC_MAX / (DISPLAY_Y_RES - DISPLAY_MENU_HEIGHT));
-	portBASE_TYPE disy = DISPLAY_X_RES - x;
+	uint16_t disx = (DISPLAY_Y_RES - DISPLAY_MENU_HEIGHT) - y / (ADC_MAX / (DISPLAY_Y_RES - DISPLAY_MENU_HEIGHT));
+	uint16_t disy = DISPLAY_X_RES - x;
 	xSemaphoreTake(lcdLock, portMAX_DELAY);
 	GLCD_putPixel(disx,disy);
 	xSemaphoreGive(lcdLock);
